d850c5696af0e6f9ef4c29f3f85c8cf5317f4d5b
XuanD <xuandeng1029@gmail.com>
2019-03-27 16:34:04 +0800

    Updates

---
+++ precache-manifest.fa3a9edf203c24eba5cf5aeb55cf19fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/RSVP-Application/static/media/header-bg.be92ab00.jpg"
+  },
+  {
+    "revision": "ce6c97b4f986820df104",
+    "url": "/RSVP-Application/static/js/runtime~main.ec0bf86e.js"
+  },
+  {
+    "revision": "3e5205c46c632db16a22",
+    "url": "/RSVP-Application/static/js/main.3eb17c05.chunk.js"
+  },
+  {
+    "revision": "0db580a5bbcaf6ff87d5",
+    "url": "/RSVP-Application/static/js/2.3fb4ccec.chunk.js"
+  },
+  {
+    "revision": "3e5205c46c632db16a22",
+    "url": "/RSVP-Application/static/css/main.1ae49f41.chunk.css"
+  },
+  {
+    "revision": "e24c8d3b74082925d38fc27abb3bf99d",
+    "url": "/RSVP-Application/index.html"
+  }
+];
